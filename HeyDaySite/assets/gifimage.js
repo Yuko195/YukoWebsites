1571eec90bf32329b7dc77c1005691db47f98da9
Yuko195 <eemtyplays@gmail.com>
2025-09-07 22:26:24 +0300

    Delete HeyDaySite directory

--- HeyDaySite/assets/gifimage.js
+++
@@ -1,9 +0,0 @@
-let img = document.getElementById('logo');
-let webpSrc = 'assets/cup.png';
-let gifSrc = 'assets/steamCup.gif';
-img.addEventListener('mouseenter', () => {
-img.src = gifSrc;
-});
-img.addEventListener('mouseleave', () => {
-img.src = webpSrc;
-});
